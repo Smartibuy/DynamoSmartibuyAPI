7212f1c72e8f8e1ae7f6e01e96ddb23609704c45
lockys <locky4567@gmail.com>
2015-12-13 23:17:32 +0800

    add good helpers

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,10 +2,13 @@ ENV['RACK_ENV'] = 'test'
 
 require 'minitest/autorun'
 require 'rack/test'
+require 'vcr'
+require 'webmock/minitest'
+
 Dir.glob('./{config,models,services,controllers}/init.rb').each do |file|
   require file
 end
-  
+
 include Rack::Test::Methods
 
 def app
