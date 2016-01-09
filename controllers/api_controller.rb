8f93a28e8fdb17eb18cadaaf74ec67fdb33a6073
qoo392 <qoo392@hotmail.com>
2016-01-09 16:34:19 +0000

    fix route

--- controllers/api_controller.rb
+++ controllers/api_controller.rb
@@ -366,9 +366,9 @@ class SmartibuyDynamo < Sinatra::Base
 
   #update&get user data
   post '/api/v1/user/:id/tag/', &add_user_hashtag
-  post '/api/v1/update_user_date/:id', &save_user_info
-  get '/api/v1/get_user_date/', &get_all_user_info
-  get '/api/v1/get_user_date/:id', &get_user_info
+  post '/api/v1/user/:id', &save_user_info
+  get '/api/v1/user/', &get_all_user_info
+  get '/api/v1/user/:id', &get_user_info
 
 
 end
